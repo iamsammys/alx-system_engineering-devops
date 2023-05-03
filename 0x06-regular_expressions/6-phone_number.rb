e1f96e086d46db1ea7c232f50b6f43165eefd718
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-05-03 23:09:20 +0100

    regex

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/h[0-9]{10}n/).join
+puts ARGV[0].scan(/\d{10}/).join
