3893fea9ff45147eeb1a3e56009c6893d6cb6971
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-06-25 14:20:36 +0100

    added a new puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # puppet script to create a configuration file
 file_line { 'key_path':
-  path => '/etc/ssh/ssh_config'
-  line => 'IdentityFile ~/.ssh/school'
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
   }
 file_line { 'password':
-  path => '/etc/ssh/ssh_config'
-  line => 'PasswordAuthentication no'
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
   }
