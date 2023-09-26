942bb619561bda504f0ef96f1968e6e2172938f2
iamsammys <samuel.ezeh.246233@unn.edu.ng>
2023-09-26 03:33:11 +0100

    CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Python script using this REST API
-"""
-from requests import get
-from sys import argv
+"""This script makes a request to a REST API"""
+
+import csv
+import requests
+import sys
 
 
-if __name__ == '__main__':
-    user_id = argv[1]
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    response = get(url)
-    username = response.json().get('username')
+if __name__ == "__main__":
+    id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_data = requests.get("{}/users/{}".format(base_url, id)).json()
+    todo_data = requests.get("{}/users/{}/todos".format(base_url, id)).json()
+    name = user_data.get('username')
 
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
-    response = get(url)
-    tasks = response.json()
-    with open('{}.csv'.format(user_id), 'w') as file:
-        for task in tasks:
-            file.write('"{}","{}","{}","{}"\n'
-                       .format(user_id, username, task.get('completed'),
-                               task.get('title')))
+    for value in todo_data:
+        data = [id, name, value.get('completed'), value.get('title')]
+        filename = "{}.csv".format(id)
+        with open(filename, 'a', encoding="utf-8") as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            writer.writerow(data)
